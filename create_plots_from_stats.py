138b598148e7ad967f1a1deeff409dc126049329
Fabian Boehm <boehmf@cip.ifi.lmu.de>
2024-04-16 15:13:38 +0200

    Final Comit before restructure

--- create_plots_from_stats.py
+++ create_plots_from_stats.py
@@ -9,32 +9,34 @@ stats = utils.read_file_to_array(r"C:\Junk\Hiwi_List\plots\benchmarking_results.
 os.chdir(plotdir)
 
 #Globals
-statcount = 7
+statcount = 14
 #End: Globals
 
-for i in range(0, 8):
-    if i % statcount == 0: continue
-    values = []
-    stat_floats = []
-    statname = ''
-    stat = utils.get_stat_from_stats(stats,i)
-    c = 0
-    while c < len(stat):
-        s = stat[c]
-        statname = s[0]
-        stat_floats.append(float(s[1].replace('%', '').strip()))
-        if c % 2 == 1:
-            values.append((stat_floats[c] + stat_floats[c - 1]) / 2)
-        c += 1
 
-    plt.bar(range(len(tool_list)), values)  # Creating a bar graph
-    plt.title(statname)  # Setting the title
-    statname = statname.replace(':','').replace('/','').strip()
-    plt.xticks(range(len(tool_list)), tool_list)  # Setting x-axis labels
-    plt.xticks(rotation= 15)
-    plt.savefig(f'{statname}.jpg')  # Saving the plot as an image file
-    plt.clf()
+def compare_tools():
+    for i in range(0, 8):
+        if i % statcount == 0: continue
+        values = []
+        stat_floats = []
+        statname = ''
+        stat = utils.get_stat_from_stats(stats,i)
+        c = 0
+        while c < len(stat):
+            s = stat[c]
+            statname = s[0]
+            stat_floats.append(float(s[1].replace('%', '').strip()))
+            if c % 2 == 1:
+                values.append((stat_floats[c] + stat_floats[c - 1]) / 2)
+            c += 1
+
+        plt.bar(range(len(tool_list)), values)  # Creating a bar graph
+        plt.title(statname)  # Setting the title
+        statname = statname.replace(':','').replace('/','').strip()
+        plt.xticks(range(len(tool_list)), tool_list)  # Setting x-axis labels
+        plt.xticks(rotation= 15)
+        plt.savefig(f'{statname}.jpg')  # Saving the plot as an image file
+        plt.clf()
 
 
-
+compare_tools()
 print('execution finished!')
