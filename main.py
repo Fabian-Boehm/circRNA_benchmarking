6635ea09369d3b4e321d5733c80e91b18920c1fb
Fabian Boehm <boehmf@cip.ifi.lmu.de>
2024-04-23 10:53:58 +0200

    attempted jaccard fix

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
         utils.create_out_directory(out_path, tool_list, args.module)
         if len(tool_list) == 1:
             tool_list = args.tool_list
-        print(tool_list)
+        print(args.tool_list)
 
         # stat computation:
         if '0' in args.module:
