c62c70921def59eed4e296544ffe383857c5061a
Fabian Boehm <boehmf@cip.ifi.lmu.de>
2024-04-23 10:52:03 +0200

    attempted jaccard fix

--- main_plots.py
+++ main_plots.py
@@ -55,6 +55,7 @@ if __name__ == "__main__":
         # stat computation:
         if '0' in args.module:
             for tool in tool_list:
+                print(tool)
                 filenames = utils.list_directories(os.path.join(args.tools_dir, tool))
                 filenames.sort()
                 written_basepairs_map = utils.get_written_basepair_map(filenames, args.trimgalore_dir)
